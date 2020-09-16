b27ff087e4870ff3413ed7ac2c6c3f9d76067b3b
maldestor95 <maldestor95@gmail.com>
2020-09-16 07:51:17 +0200

    chore(mocha): init TDD

---
+++ src/__tests__/buildlink.spec.ts
@@ -0,0 +1,8 @@
+import * as buildLink from "../buildlink"
+import {expect} from "chai"
+describe('buildLink',()=>{
+    it('shall pass',done=>{
+        expect(true).to.eq(true)
+        done()
+    })
+})
